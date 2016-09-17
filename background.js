324cd92b3809229f8a0262a72f0c00c30815bd3b
Arjun Kalburgi <askalburgi@gmail.com>
2016-09-17 15:26:39 -0400

    popup.hhtml populated

--- background.js
+++ background.js
@@ -1,71 +1,75 @@
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-	console.log('Search Spotify for: ' + text);
-	chrome.omnibox.setDefaultSuggestion({
-		description: "Press Enter to search Spotify"
-	});
-
-
-	var Spotify = require(['spotify-web-api']);
-	var s = new Spotify();
-	
-	var spotifyApi = new SpotifyWebApi();
-	chrome.storage.sync.get("AccessToken", (StorageObj) => {
- 					spotifyApi.setAccessToken(StorageObj.AccessToken);
-
- 		    			console.log(StorageObj.AccessToken);
- 	    		 });
-	var prev=null;
-	
-	if (prev !== null) {
-    prev.abort();
-  	}
-	prev = spotifyApi.searchTracks(text, {limit: 5})
-    .then(function(data) {
-
-    	console.log(data);
-
-      // clean the promise so it doesn't call abort
-      function encodeXml(s) {
-  		var holder = document.createElement('div');
-  		holder.textContent = s;
-  		return holder.innerHTML;
-		}
-      var suggestions=[];
-      for (i=0;i<data.tracks.items.length;i++){
-      	str1=data.tracks.items[i].name.concat(' by ').concat(data.tracks.items[i].artists['0'].name);
-		suggestions.push({content:encodeXml(str1), description:encodeXml(str1)});
-      	console.log(str1);
-      }
-      prev = null;
-      if(suggestions!=null){
-      	suggest(suggestions);
-  	  }
-       // ...render list of search results...
-
-    }, function(err) {
-    	console.log('im here');
-      	console.error(err);
+    console.log('Search Spotify for: ' + text);
+    chrome.omnibox.setDefaultSuggestion({
+        description: "Press Enter to search Spotify"
     });
 
 
-	/*
-	// console.log('inputChanged: ' + text);
+    var Spotify = require(['spotify-web-api']);
+    var s = new Spotify();
+
+    var spotifyApi = new SpotifyWebApi();
+    chrome.storage.sync.get("AccessToken", (StorageObj) => {
+        spotifyApi.setAccessToken(StorageObj.AccessToken);
+        console.log(StorageObj.AccessToken);
+    });
+
+    var prev = null;
+    if (prev !== null) {
+        prev.abort();
+    }
+    prev = spotifyApi.searchTracks(text, {
+            limit: 5
+        })
+        .then(function(data) {
+
+            console.log(data);
+
+            // clean the promise so it doesn't call abort
+            function encodeXml(s) {
+                var holder = document.createElement('div');
+                holder.textContent = s;
+                return holder.innerHTML;
+            }
+            var suggestions = [];
+            for (i = 0; i < data.tracks.items.length; i++) {
+                str1 = data.tracks.items[i].name.concat(' by ').concat(data.tracks.items[i].artists['0'].name);
+                suggestions.push({
+                    content: encodeXml(str1),
+                    description: encodeXml(str1)
+                });
+                console.log(str1);
+            }
+            prev = null;
+            if (suggestions != null) {
+                suggest(suggestions);
+            }
+            // ...render list of search results...
+
+        }, function(err) {
+            console.log('im here');
+            console.error(err);
+        });
+
+
+    /*
+    // console.log('inputChanged: ' + text);
 	
-	// First suggest line
-	var char_remain = new String();
-	if (text.length<=140){
-			char_remain="Press Enter to Tweet - " + String(140-text.length) + " characters remaining";
-		}
-		else {
-			char_remain="Too Long!";
-		}
-	chrome.omnibox.setDefaultSuggestion({
-		description: char_remain
-	});
+    // First suggest line
+    var char_remain = new String();
+    if (text.length<=140){
+    		char_remain="Press Enter to Tweet - " + String(140-text.length) + " characters remaining";
+    	}
+    	else {
+    		char_remain="Too Long!";
+    	}
+    chrome.omnibox.setDefaultSuggestion({
+    	description: char_remain
+    });
 	
-	// Other suggest lines
-	*/
-	/*var suggestions = [];
+    // Other suggest lines
+    */
+    /*var suggestions = [];
 	suggestions.push({ content: 'Coffee - Wikipedia', description: 'Coffee - Wikipedia' });
         suggestions.push({ content: 'Starbucks Coffee', description: 'Starbucks Coffee' });
 	suggest(suggestions);*/
@@ -74,58 +78,56 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(function(text) {
 
-	console.log('HERE');
-	
 
-	var Spotify = require(['spotify-web-api']);
-	var s = new Spotify();
-	
-	var spotifyApi = new SpotifyWebApi();
-	chrome.storage.sync.get("AccessToken", (StorageObj) => {
- 					spotifyApi.setAccessToken(StorageObj.AccessToken);
-
- 		    			console.log(StorageObj.AccessToken);
- 	    		 });
-	
-	prev = spotifyApi.searchTracks(text, {limit: 5})
-    .then(function(data) {
-
-    	console.log(data);
-
-      // clean the promise so it doesn't call abort
-      prev = null;
-
-      // ...render list of search results...
-
-    }, function(err) {
-      console.error(err);
+	// set up interfacing 
+    var Spotify = require(['spotify-web-api']);
+    var s = new Spotify();
+    var spotifyApi = new SpotifyWebApi();
+    chrome.storage.sync.get("AccessToken", (StorageObj) => {
+        spotifyApi.setAccessToken(StorageObj.AccessToken);
     });
 
-	// get Elvis' albums, passing a callback. When a callback is passed, no Promise is returned
-	/*spotifyApi.getArtistAlbums(text, function(err, data) {
+    // tracks data
+    prev = spotifyApi.searchTracks(text, {
+            limit: 5
+        })
+        .then(function(data) {
+            console.log("Spotify searchTracks");
+            console.log(data);
+            // clean the promise so it doesn't call abort
+            prev = null;
+
+            // ...render list of search results...
+
+        }, function(err) {
+            console.error(err);
+        });
+
+    // get Elvis' albums, passing a callback. When a callback is passed, no Promise is returned
+    /*spotifyApi.getArtistAlbums(text, function(err, data) {
   	if (err) console.error(err);
   	else console.log('Artist albums', data);
 	});*/
-	/*
-	// If the tweet goes over 140 characters send a notification
-	if (Notification.permission !== "granted") {
-		// 
-		Notification.requestPermission();
-	}
-	if (no_of_char>140) {
-		var notification = new Notification('Character limit exceeded', {
-			icon: 'ShoutBox128.png',
-			body: "Oops! You're tweet was too long, try again!",
-		});
-		notification.onclick = function () {
-			window.open("http://twitter.com/");
-		}
-	}
+    /*
+    // If the tweet goes over 140 characters send a notification
+    if (Notification.permission !== "granted") {
+    	// 
+    	Notification.requestPermission();
+    }
+    if (no_of_char>140) {
+    	var notification = new Notification('Character limit exceeded', {
+    		icon: 'ShoutBox128.png',
+    		body: "Oops! You're tweet was too long, try again!",
+    	});
+    	notification.onclick = function () {
+    		window.open("http://twitter.com/");
+    	}
+    }
 
-	// Post using Twitter shit
-	var xmlhttp=new XMLHttpRequest();
-	// xmlhttp.open("GET","http://127.0.0.1:3000/post-status?message=" +  encodeURIComponent(text),true); // for when testing
-	xmlhttp.open("GET","https://shoutboxextension.herokuapp.com/post-status?message=" +  encodeURIComponent(text),true); // for when deployed
-	xmlhttp.send();
-	*/
+    // Post using Twitter shit
+    var xmlhttp=new XMLHttpRequest();
+    // xmlhttp.open("GET","http://127.0.0.1:3000/post-status?message=" +  encodeURIComponent(text),true); // for when testing
+    xmlhttp.open("GET","https://shoutboxextension.herokuapp.com/post-status?message=" +  encodeURIComponent(text),true); // for when deployed
+    xmlhttp.send();
+    */
 });
--- callback.js
+++ callback.js
@@ -1,9 +1,85 @@
-window.onload = () => {
+var data; 
 
+function populatePlaylistTracks(spotifyApi) {
+	console.log("populatePlaylistTracks"); 
+	// prev = spotifyApi.getPlaylistTracks("kanye", {
+	prev = spotifyApi.getPlaylistTracks("askalburgionspotify", "5poflllJE2p9koyxXWd1Xr", {})
+		.then(function(data) {
+		    console.log(data);
+		 	for (var i =0; i<data.items.length; i++) {
+		 		var tbody = document.getElementById("tracklisting"); 
+		 		var tr = document.createElement("tr");
+				tbody.appendChild(tr);
+
+		 		var tdplay = document.createElement("td"); 
+		 		tr.appendChild(tdplay); 
+		 		var buttonplay = document.createElement("button"); 
+		 		tdplay.appendChild(buttonplay);
+
+		 		var tdsong = document.createElement("td"); 
+		 		tr.appendChild(tdsong); 
+		 		tdsong.innerHTML = data.items[i].track.name;
+
+		 		var tdartist = document.createElement("td"); 
+		 		tr.appendChild(tdartist); 
+		 		tdartist.innerHTML = data.items[i].track.artists[0].name;
+
+		 		var tdalbum = document.createElement("td"); 
+		 		tr.appendChild(tdalbum); 
+		 		tdalbum.innerHTML = data.items[i].track.album.name;
+
+		 		var tdtime = document.createElement("td"); 
+		 		tr.appendChild(tdtime); 
+		 		var ms = data.items[i].track.duration_ms;
+		 		tdtime.innerHTML = Math.floor(ms/1000/60) + ":" + Math.floor((ms/1000)%60); 
+
+		 		var tdremove = document.createElement("td"); 
+		 		tr.appendChild(tdremove); 
+		 		var buttonremove = document.createElement("button"); 
+		 		tdremove.appendChild(buttonremove);
+
+		 		// data.items[i].track
+		 	}
+
+		}, function(err) {
+		    console.log('im here');
+		    console.error(err);
+		});
+	console.log("end"); 
+}
+
+
+
+window.onload = () => {
+	// configure popup.html
+	chrome.storage.sync.get("AccessToken", (StorageObj) => {
+		var user = document.getElementsByClassName("user"); 
+		var nouser = document.getElementsByClassName("nouser"); 
+		if (StorageObj.AccessToken) {
+			// page css
+			for (var i=0; i<nouser.length; i++) {
+				// nouser[i].style.display = "none";
+			}
+			
+			// add spotify
+			var Spotify = require(['spotify-web-api']);
+			var s = new Spotify();
+			var spotifyApi = new SpotifyWebApi();
+			spotifyApi.setAccessToken(StorageObj.AccessToken);
+
+			// populate 
+			populatePlaylistTracks(spotifyApi); 
+		} else {
+			for (var i=0; i<user.length; i++) {
+				user[i].style.display = "none";
+			}
+		}
+	})
+
+	// oauth flow
 	var callbackurl = encodeURIComponent(chrome.identity.getRedirectURL() + "callback.html"); 
 	var URL = "https://accounts.spotify.com/en/authorize?client_id=7bd298c4b0bd46499b47b9acfbfd6671&redirect_uri=" + callbackurl + "&response_type=token"
-
- 	document.onclick = () => {
+ 	document.getElementById("signin").onclick = () => {
  		// console.log(callbackurl); 
  	    chrome.identity.launchWebAuthFlow({
  	   		url:URL, 
@@ -12,11 +88,7 @@ window.onload = () => {
  	    	// console.log(url);
  	    	chrome.storage.sync.set({'AccessToken':url.substring(url.indexOf("=")+1,url.indexOf("&"))}, () => {
  	    		console.log("AccessToken saved"); 
- 	    		// chrome.storage.sync.get("AccessToken", (AT) => {
- 	    		//	console.log(AT.AccessToken);
- 	    		// });
  	    	}); 
  	    });
-
     }
- };
+};
