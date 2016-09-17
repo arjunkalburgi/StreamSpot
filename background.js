c2f6f63c0f8ec1f000bf84aa3ed7e4ad954da707
HarryPahwa <harrypahwa95@gmail.com>
2016-09-17 09:07:22 -0400

    made changes to background.js and defined it in the manifest
    Still contains elements from shoutbox

--- background.js
+++ background.js
@@ -1,5 +1,12 @@
 chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
-	
+	console.log('Search Spotify for: ' + text);
+	chrome.omnibox.setDefaultSuggestion({
+		description: "Press Enter to search Spotify"
+	});
+
+
+
+	/*
 	// console.log('inputChanged: ' + text);
 	
 	// First suggest line
@@ -15,6 +22,7 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
 	});
 	
 	// Other suggest lines
+	*/
 	suggest([
 		{content: text + " ", description: "Use mentions - Type '@' followed by a twitter handle."},
 		{content: text + " ", description: "Paste in links - They will automatically shorten."}
@@ -24,10 +32,21 @@ chrome.omnibox.onInputChanged.addListener(function(text, suggest) {
 // This event is fired with the user accepts the input in the omnibox.
 chrome.omnibox.onInputEntered.addListener(function(text) {
 
-	console.log('Search Spotify for: ' + text);
+	console.log('HERE');
+	
 
 	var Spotify = require('spotify-web-api-js');
 	var s = new Spotify();
+	
+	var spotifyApi = new SpotifyWebApi();
+
+	spotifyApi.setAccessToken('<here_your_access_token>');
+
+	// get Elvis' albums, passing a callback. When a callback is passed, no Promise is returned
+	spotifyApi.getArtistAlbums('43ZHCT0cAZBISjO8DG9PnE', function(err, data) {
+  	if (err) console.error(err);
+  	else console.log('Artist albums', data);
+	});
 	/*
 	// If the tweet goes over 140 characters send a notification
 	if (Notification.permission !== "granted") {
