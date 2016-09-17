a09fce0be95e60389831f15e673aceb860d06447
Arjun Kalburgi <askalburgi@gmail.com>
2016-09-17 09:02:18 -0400

    added ability to get AccessToken (in comment)

--- callback.js
+++ callback.js
@@ -4,19 +4,19 @@ window.onload = () => {
 	var URL = "https://accounts.spotify.com/en/authorize?client_id=7bd298c4b0bd46499b47b9acfbfd6671&redirect_uri=" + callbackurl + "&response_type=token"
 
  	document.onclick = () => {
- 		console.log(callbackurl); 
+ 		// console.log(callbackurl); 
  	    chrome.identity.launchWebAuthFlow({
  	   		url:URL, 
  	   		interactive:true
  	    }, (url) => {
- 	    	console.log(url);
- 	    	chrome.storage.sync.set({'AccessToken':url}, () => {
+ 	    	// console.log(url);
+ 	    	chrome.storage.sync.set({'AccessToken':url.substring(url.indexOf("=")+1,url.indexOf("&"))}, () => {
  	    		console.log("AccessToken saved"); 
+ 	    		// chrome.storage.sync.get("AccessToken", (AT) => {
+ 	    		//	console.log(AT.AccessToken);
+ 	    		// });
  	    	}); 
  	    });
 
     }
  };
-
-  	   			// redirect_uri=" + https%3A%2F%2Fmcbheecpcnnheoaobnblgghppjhcilbc.chromiumapp.org%2Fcallback.html + "&\
-									
