12e1f1b4b2f705e45c040994e840506467a1d4f1
Arjun Kalburgi <askalburgi@gmail.com>
2016-09-17 08:41:39 -0400

    url fix and saving of accesstoken

--- callback.js
+++ callback.js
@@ -1,15 +1,22 @@
 window.onload = () => {
 
 	var callbackurl = encodeURIComponent(chrome.identity.getRedirectURL() + "callback.html"); 
+	var URL = "https://accounts.spotify.com/en/authorize?client_id=7bd298c4b0bd46499b47b9acfbfd6671&redirect_uri=" + callbackurl + "&response_type=token"
 
  	document.onclick = () => {
  		console.log(callbackurl); 
  	    chrome.identity.launchWebAuthFlow({
- 	   		url:"https://accounts.spotify.com/en/authorize?client_id=7bd298c4b0bd46499b47b9acfbfd6671&redirect_uri=https%3A%2F%2Fmcbheecpcnnheoaobnblgghppjhcilbc.chromiumapp.org%2Fcallback.html&response_type=token", 
- 	   		// url:"https://accounts.spotify.com/en/authorize?client_id=7bd298c4b0bd46499b47b9acfbfd6671&redirect_uri=chrome-extension:%2F%2Fmcbheecpcnnheoaobnblgghppjhcilbc%2Fcallback.html&response_type=token", 
- 	   		// url:"https://accounts.spotify.com/en/authorize?client_id=7bd298c4b0bd46499b47b9acfbfd6671&redirect_uri=http:%2F%2Fexample.com%2Fcallback&response_type=token", 
+ 	   		url:URL, 
  	   		interactive:true
- 	    }, (url) => console.log(url));
+ 	    }, (url) => {
+ 	    	console.log(url);
+ 	    	chrome.storage.sync.set({'AccessToken':url}, () => {
+ 	    		console.log("AccessToken saved"); 
+ 	    	}); 
+ 	    });
 
     }
  };
+
+  	   			// redirect_uri=" + https%3A%2F%2Fmcbheecpcnnheoaobnblgghppjhcilbc.chromiumapp.org%2Fcallback.html + "&\
+									
