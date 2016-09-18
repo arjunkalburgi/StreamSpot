30efbdbb406cf6fb69871e75aecaf8ed7e69d550
HarryPahwa <harrypahwa95@gmail.com>
2016-09-18 03:25:45 -0400

    MUSIC PLAYSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS!!!!!!!!!!!!!!

--- background.js
+++ background.js
@@ -125,7 +125,74 @@ chrome.omnibox.onInputEntered.addListener(function(text) {
     console.error(err);
   });
 
+  	var audiotag = new Audio();
+  	var _playing = false;
+		var _track = '';
+		var _volume = 100;
+		var _progress = 0;
+		var _duration = 0;
+		var _trackdata = null;
 
+		function tick() {
+			if (!_playing) {
+				return;
+			}
+			_progress = audiotag.currentTime * 1000.0;
+
+			$rootScope.$emit('trackprogress');
+			
+		}
+
+		var ticktimer = 0;
+
+		function enableTick() {
+			disableTick();
+			ticktimer = $interval(tick, 100);
+		}
+
+		function disableTick() {
+			if (ticktimer != 0) {
+				$interval.cancel(ticktimer);
+			}
+		}
+
+	function createAndPlayAudio(url, callback, endcallback) {
+			console.log('createAndPlayAudio', url);
+			if (audiotag.src != null) {
+				audiotag.pause();
+				audiotag.src = null;
+			}
+			audiotag.src = url;
+			audiotag.addEventListener('loadedmetadata', function() {
+				console.log('audiotag loadedmetadata');
+				_duration = audiotag.duration * 1000.0;
+				audiotag.volume = _volume / 100.0;
+				audiotag.play();
+				callback();
+			}, false);
+			audiotag.addEventListener('ended', function() {
+				console.log('audiotag ended');
+				_playing = false;
+				_track = '';
+				disableTick();
+				$rootScope.$emit('endtrack');
+			}, false);
+		}
+		
+
+				
+			
+
+  	spotifyApi.getTrack(text).then(function(trackdata) {
+					console.log('playback got track', trackdata);
+					createAndPlayAudio(trackdata.preview_url, function() {
+						 _trackdata = trackdata;
+						 _progress = 0;
+						 $rootScope.$emit('playerchanged');
+						 $rootScope.$emit('trackprogress');
+						 enableTick();
+					});
+				});
 
 
     /*spotifyApi.getUserPlaylists('askalburgionspotify')
